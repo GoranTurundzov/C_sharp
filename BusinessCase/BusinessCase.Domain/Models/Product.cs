05ea36ebd05f22c45bdb3c22f10520acc09f17a4
GoranTurundzov <77289944+GoranTurundzov@users.noreply.github.com>
2021-06-19 19:13:40 +0200

    business

---
+++ BusinessCase/BusinessCase.Domain/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using BusinessCase.Domain.Interfaces;
+using System;
+
+namespace BusinessCase.Domain
+{
+    public class Product : IProduct
+    {
+
+        public string Name { get; set; }
+        public bool Domestic { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public double? Weight { get; set; }
+
+    }
+}

fe21784aa46bfaf284fd4647dc273520751bbfc0
GoranTurundzov <77289944+GoranTurundzov@users.noreply.github.com>
2021-06-19 19:08:54 +0200

    asd

---
+++ BusinessCase/BusinessCase.App/Program.cs
@@ -0,0 +1,18 @@
+﻿using BusinessCase.Services;
+using System;
+
+namespace BusinessCase.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintingServices.FillList();
+
+             Console.WriteLine(PrintReceiptService.PrintBill());
+
+
+      
+        }
+    }
+}
---
+++ BusinessCase/BusinessCase.Domain/Interfaces/IProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCase.Domain.Interfaces
+{
+    public interface IProduct
+    {
+        string Name { get; set; }
+        bool Domestic { get; set; }
+        double Price { get; set; }
+        string Description { get; set; }
+        double? Weight { get; set; }
+    }
+}
